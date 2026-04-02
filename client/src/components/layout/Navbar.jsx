e1d633a7c60c43bbbf6153b4f3168506d0c2bbbf
Zouidi Lyna <masterstudentzl1@gmail.com>
2026-04-02 13:02:36 +0100

    update logo

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -26,6 +26,7 @@ export default function Navbar() {
     alt="AgroSmart DZ" 
     style={{ height: '45px', objectFit: 'contain' }} 
   />
+   AgroSmart <span>DZ</span>
 </Link>
 
       {/* DESKTOP LINKS */}
